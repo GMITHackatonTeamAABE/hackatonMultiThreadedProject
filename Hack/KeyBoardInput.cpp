c440aa29e0615cd5b15292d75bd59aee750d800b
EoghanGibbons <Eoghanfrommayo@live.com>
2015-11-11 14:59:54 +0000

    helloWorld

---
+++ Hack/KeyBoardInput.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include "KeyboardInput.h"
+
+
+bool KeyBoardInput::instanceFlag = false;
+KeyBoardInput* KeyBoardInput::instance = NULL;
+
+
+KeyBoardInput* KeyBoardInput::GetInstance()
+{
+	if (!instanceFlag)
+	{
+		instance = new KeyBoardInput;
+		instanceFlag = true;
+		return instance;
+	}
+	else
+	{
+		return instance;
+	}
+}
+void KeyBoardInput::updateKeyboard(SDL_Event e)
+{
+	//User presses a key
+	if (e.type == SDL_KEYDOWN)
+	{
+		pressedKeys.push_back(e.key.keysym.sym);
+	}
+}
+void KeyBoardInput::clearKeys()
+{
+	pressedKeys.clear();
+}
+bool KeyBoardInput::isKeyPressed(SDL_Keycode key)
+{
+	for each (SDL_Keycode pressed in pressedKeys)
+	{
+		if (key == pressed)
+		{
+			return true;
+		}
+	}
+	return false;
+}
